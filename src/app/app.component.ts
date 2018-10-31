19bcf86dd64699a2f0322329ebf2dfcb7e78bdc9
Marketa Skoloudikova <marketa.skoloudikova@forcemanager.net>
2018-10-31 12:30:25 +0100

    'init'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
-
+import {ExcelService} from './services/excel.service';
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular-file-exporter-demo';
+  name = 'Angular 6'
+  data: any = [{
+    Check: 'e101',
+    Id: 'ravi',
+    FechaRegistro: 1000,
+    Sala: 1,
+    Título: 'A',
+    TiempoPractica: 'test',
+    Mejora: 'test',
+    Incremento: 'x',
+    Formalización: 'gaidgiadgia',
+    Retencion: 'dbgaisdgiadg',
+    motivacional: 'hdiadhiadhiad'
+     
+    },
+    {
+    Check: 'e102',
+    Id: 'ram',
+    FechaRegistro: 2000
+    },
+    {
+    Check: 'e103',
+    Id: 'rajesh',
+    FechaRegistro: 3000
+    }
+  ];
+  constructor(private excelService:ExcelService){}
+
+  exportAsXLSX():void {
+    this.excelService.exportAsExcelFile(this.data, 'sample');
+  }
 }
